4d968ec5db01997c1126bbf665e56a6e09583332
Дмитрий ПК <igra123.24@mail.ru>
2023-09-28 19:42:26 +0300

    Домашний ПК. Начало прилы

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,27 @@
 package ru.practicum.shareit.item.model;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+import ru.practicum.shareit.user.User;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Item {
+
+    long id;
+    @NotBlank
+    @NotNull
+    String title;
+    @Size(max = 200, message = "Максимальная длинна описания 200 символов")
+    @NotNull
+    String description;
+    @NotNull
+    boolean available;
+    @NotNull
+    User owner;
 }
--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,24 @@
 package ru.practicum.shareit.user;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class User {
+
+    long id;
+    @NotNull
+    @NotBlank
+    String name;
+    @Email(message = "Неправильный адрес электронной почты")
+    @NotEmpty(message = "Поле email не может быть пустым")
+    @NotNull
+    private String email;
 }
