321a2cb5a9a2a0fd6d9bf1897dacb6c05f3006b2
impossible <igra123.24@mail.ru>
2023-10-20 14:59:08 +0300

    Поправил код, сделал отдельный пакет с классами валидации.

--- src/main/java/ru/practicum/shareit/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/mapper/ItemMapper.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.user.dao.UserStorage;
 import ru.practicum.shareit.user.model.User;
 
 @Component
